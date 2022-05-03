ead3a0c5225d94c0ce3ac93c9c2a148dc18a2db9
Dario La Tegola <dariolategola@gmail.com>
2022-05-03 15:58:54 +0200

    Creato seeder per amenities

---
+++ database/seeds/AmenitiesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Amenity;
+use Illuminate\Database\Seeder;
+
+class AmenitiesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $amenities = ['Wi-Fi', 'Posto macchina', 'Portineria', 'Sauna', 'Vista mare'];
+
+        foreach ($amenities as $amenity) {
+            $newAmenity = new Amenity();
+            $newAmenity->name = $amenity;
+            $newAmenity->save();
+        }
+    }
+}
