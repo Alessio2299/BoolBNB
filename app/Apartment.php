623dfb82d78e8d4d49f9b7aab9fec488c1b3ab3b
Dario La Tegola <dariolategola@gmail.com>
2022-05-04 15:40:24 +0200

    Create relazioni molti a molti tra apartments e amenities

--- app/Apartment.php
+++ app/Apartment.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Apartment extends Model
 {
     protected $fillable = ['title', 'description', 'rooms', 'beds', 'bathrooms', 'square_meters', 'image', 'availability', 'street', 'civic_number', 'zip_code', 'city', 'country', 'slug'];
+
+    public function amenities() {
+        return $this->belongsToMany('App\Amenity');
+    }
 }
 
