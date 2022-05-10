07f1865857b53893aa0a2a44b0fc3570f275b4dc
Alessio Napoli <alessionapoli99@icloud.com>
2022-05-10 14:34:34 +0200

    Creato Footer

--- resources/views/Guests/home.blade.php
+++ resources/views/Guests/home.blade.php
@@ -6,6 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home Guest</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ms+Madi&display=swap" rel="stylesheet">
   </head>
   <body>
     <div id="root">
