16df70619780ab0c93289cf925b27269680b8fa9
Alessio Napoli <alessionapoli99@icloud.com>
2022-05-11 17:12:45 +0200

    Add Map

--- resources/js/pages/About.vue
+++ resources/js/pages/About.vue
@@ -48,7 +48,7 @@
   export default {
     name : 'Home',
     components:{
-      CardTeam
+      CardTeam,
     }
   }
 </script>
---
+++ resources/js/pages/partials/MapFeature.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id='map' class='map'></div>
+</template>
+
+<script>
+  import tt from '@tomtom-international/web-sdk-maps';
+  export default {
+    name : 'MapFueture',
+    data() {
+      return {
+      }
+    },
+    mounted(){
+      this.initializeMap()
+    },
+    methods:{
+      initializeMap() { 
+        this.map = tt.map({
+        key: 'TounQy5Lqgw3CSCowM1qIL48LHEGF6WA',
+        container: 'map',
+        zoom: 17
+        });
+      }
+    }    
+  }
+</script>
+
+<style lang="scss" scoped>
+  #map{
+    width: 60vw;
+    height: 60vh;
+  }
+</style>>
