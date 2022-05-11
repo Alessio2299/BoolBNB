0e6cabe5de68ca071f0f67c6a37adad0580580a0
Edoardo Lai <edoardolai96@gmail.com>
2022-05-11 11:11:36 +0200

    Divisione sezioni in componenti

---
+++ resources/js/pages/partials/Carousel.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container-fluid py-4" id="jumbotron" style="backgroundColor: cornflowerblue">
+      <div class="row debug mx-5">
+        <div class="col debug text-center d-flex flex-column justify-content-around">
+          <h1 class="text-center">Carousel</h1>
+
+          <div>
+            <button type="button" class="btn btn-outline-dark">Bottone per azione</button>
+          </div>
+
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+  
+  export default {
+    name: 'Carousel',
+    data(){
+      return{
+
+      }
+    },
+    components:{
+     
+    }
+  }
+</script>
+
+<style>
+
+</style>
