fdc6ef720cbd9da6a277951e063b2def296c1658
Dario La Tegola <dariolategola@gmail.com>
2022-05-11 16:33:53 +0200

    modifiche

--- resources/js/pages/partials/SingleApartment.vue
+++ resources/js/pages/partials/SingleApartment.vue
@@ -19,12 +19,19 @@
         <div class="row row-cols-2">
             <!-- Colonna immagine appartamento -->
             <div class="col">
-                <img class="rounded img-fluid" src="https://via.placeholder.com/350">
+                <h3>Foto dell'appartamento</h3>
+                <img class="img-fluid p-3" src="https://via.placeholder.com/350">
             </div>
             <!-- Colonna immagine appartamento -->
             <!-- Colonna mappa -->
             <div class="col">
-                <img class="rounded img-fluid" src="https://via.placeholder.com/350">
+                <div class="map">
+                    <h3 class="text-center p-5">Possibile collocazione mappa</h3>
+                </div>
+                <div>
+                    <h4>Highlights near you</h4>
+                    <p>Descrizione dei maggiori punti di interesse vicini all'indirizzo</p>
+                </div>
             </div>
             <!-- Colonna mappa -->
         </div>
@@ -41,6 +48,12 @@
             </div>
             <div class="col">
                 <p>Box con servizi di maggior interesse</p>
+                <ul>
+                    <li>Ex: Check-in time</li>
+                    <li>Ex: Check-out time</li>
+                    <li>Ex: no smoking area</li>
+                    <li>Ex: not suitabile for kids</li>
+                </ul>
             </div>
         </div>
         <!-- Row informazioni appartamento -->
@@ -48,6 +61,7 @@
         <!-- Row servizi aggiuntivi -->
         <div class="row">
             <div class="col">
+                <h3>Additional services for enjoying your stay</h3>
                 <ul>
                     <li>Servizio 1</li>
                     <li>Servizio 2</li>
@@ -92,6 +106,16 @@ export default {
 
         .col {
             border: 1px solid black;
+        }
+
+        .map {
+            height: 80%;
+            width: 100%;
+            border: 1px solid blue;
+        }
+
+        img {
+            border-radius: 10%;
         }
     }
 </style>
