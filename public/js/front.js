7e4daf805884abaf4f93a6eea14a73db048be028
Edoardo Lai <edoardolai96@gmail.com>
2022-05-16 15:18:06 +0200

    .

--- public/js/front.js
+++ public/js/front.js
@@ -23374,7 +23374,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/alessionapoli/Desktop/BooBNB/resources/js/front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! /Users/edoardo/Desktop/progetto_finale_boolean/BoolBnb/resources/js/front.js */"./resources/js/front.js");
 
 
 /***/ })
