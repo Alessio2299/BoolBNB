1f847019c58c64a1657e9d580e5a652d55775fb9
Marianna Galuppi <marianna.galuppi@gmail.com>
2022-05-19 02:11:01 +0700

    About + AboutSecond

--- resources/js/pages/About.vue
+++ resources/js/pages/About.vue
@@ -2,8 +2,10 @@
   <div>
     <section id="head">
       <div class="container">
-        <h1 class="title up">made by travellers</h1>
-        <h1 class="title down">for travellers</h1>
+        <div class="titles-wrap">
+          <h1 class="title up">made by travellers</h1>
+          <h1 class="title down">for travellers</h1>
+        </div>
         <div class="go-to-wrap">
           <span>about us</span>
           <a class="go-to-mission" href="#mission"><i class="fas fa-chevron-down"></i></a>
@@ -13,20 +15,20 @@
 
     <section id="mission">
       <div class="container">
-        <div class="row">
-          <div class="col-4">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 ">
+          <div class="col">
             <div class="img-wrap">
-              <img src="../../../storage/app/public/img/time.jpg" alt="time illustration">
+              <img src="../../../storage/app/public/img/time-2.jpg" alt="time picture">
             </div>
             <div class="text-wrap">
               <h3>It's about time.</h3>
-              <p>Time is our most precious, non-renewable resource. By helping you to handle bookings, we are giving you the time and headspace to be their greatest self in the greatest spots.</p>
+              <p>Time is our most precious, non-renewable resource. By helping you to handle bookings, we are giving you the time and headspace to be your greatest self in the greatest spots.</p>
             </div>
           </div>
 
-          <div class="col-4">
+          <div class="col">
             <div class="img-wrap">
-              <img src="../../../storage/app/public/img/choices.jpg" alt="choices illustration">
+              <img src="../../../storage/app/public/img/choices-2.jpeg" alt="choices picture">
             </div>
             <div class="text-wrap">
               <h3>It’s about choice.</h3>
@@ -34,12 +36,12 @@
             </div>
           </div>
 
-          <div class="col-4">
+          <div class="col">
             <div class="img-wrap">
-              <img src="../../../storage/app/public/img/connectivity.jpg" alt="connectivity illustration">
+              <img src="../../../storage/app/public/img/connection-2.jpeg" alt="connectivity picture">
             </div>
             <div class="text-wrap">
-              <h3>It’s about connectivity.</h3>
+              <h3>It’s about connection.</h3>
               <p>Hospitality is about building relationships of trust. Relationships are the foundation for community, which gives meaning to the places we call home.</p>
             </div>
           </div>
@@ -48,16 +50,13 @@
       </div>
     </section>
 
-    <section id="story">
-      <div class="background-wrap">
+    <section id="journey">
+      <div class="left"></div>
 
-      </div>
-      <div class="container">
-        <div class="row">
-          <div class="col-7">
-            <h2 class="title">our story</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non sodales neque sodales ut etiam. Eleifend donec pretium vulputate sapien nec. Arcu risus quis varius quam quisque id. Mauris sit amet massa vitae tortor condimentum lacinia quis vel. Vehicula ipsum a arcu cursus vitae congue mauris. Duis tristique sollicitudin nibh sit amet commodo nulla. Facilisis volutpat est velit egestas dui id ornare arcu odio. Vitae aliquet nec ullamcorper sit amet risus nullam eget. Sapien eget mi proin sed libero enim. Euismod lacinia at quis risus sed vulputate odio. Tempus egestas sed sed risus pretium quam vulputate. Id diam vel quam elementum. Sagittis aliquam malesuada bibendum arcu vitae elementum curabitur. Fermentum et sollicitudin ac orci phasellus. Justo donec enim diam vulputate ut. Sagittis purus sit amet volutpat consequat. Et egestas quis ipsum suspendisse. Bibendum neque egestas congue quisque egestas diam in arcu. Nibh sit amet commodo nulla facilisi nullam. Nulla facilisi etiam dignissim diam quis enim lobortis. In tellus integer feugiat scelerisque varius morbi enim nunc faucibus. Vel pretium lectus quam id leo in vitae.</p>
-          </div>
+      <div class="right">
+        <div class="text-wrap">
+          <h2 class="title">our journey</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non sodales neque sodales ut etiam. Eleifend donec pretium vulputate sapien nec. Arcu risus quis varius quam quisque id. Mauris sit amet massa vitae tortor condimentum lacinia quis vel. Vehicula ipsum a arcu cursus vitae congue mauris. Duis tristique sollicitudin nibh sit amet commodo nulla. Facilisis volutpat est velit egestas dui id ornare arcu odio. Vitae aliquet nec ullamcorper sit amet risus nullam eget. Sapien eget mi proin sed libero enim. Euismod lacinia at quis risus sed vulputate odio. Tempus egestas sed sed risus pretium quam vulputate. Id diam vel quam elementum. Sagittis aliquam malesuada bibendum arcu vitae elementum curabitur.</p>
         </div>
       </div>
     </section>
@@ -65,88 +64,55 @@
     <section id="founders">
       <div class="container">
         <div class="row">
-          <div class="col-5">
-            <div class="img-wrap">
-              <img src="../../../storage/app/public/img/founder-m.jpg" alt="founder">
-            </div>
-
-            <div class="text-wrap">
-              <h3>Founder A</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non sodales neque sodales ut etiam. Eleifend donec pretium vulputate sapien nec. Arcu risus quis varius quam quisque id. Mauris sit amet massa vitae tortor condimentum lacinia quis vel. Vehicula ipsum a arcu cursus vitae congue mauris. Duis tristique sollicitudin nibh sit amet commodo nulla. Facilisis volutpat est velit egestas dui id ornare arcu odio. Vitae aliquet nec ullamcorper sit amet risus nullam eget. Sapien eget mi proin sed libero enim. Euismod lacinia at quis risus sed vulputate odio. Tempus egestas sed sed risus pretium quam vulputate. Id diam vel quam elementum. Sagittis aliquam malesuada bibendum arcu vitae elementum curabitur.</p>
-            </div>
+          <div class="col-12">
+            <h1>Meet the founders</h1>
           </div>
-
-          <div class="col-5">
+        </div>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3">
+          <div class="col">
+            <div class="img-wrap">
+                <img src="../../../storage/app/public/img/founder-m.jpg" alt="founder">
+            </div>
+            <h3>Mark Pills</h3>
+          </div>
+          <div class="col">
             <div class="img-wrap">
               <img src="../../../storage/app/public/img/founder-f.jpg" alt="founder">
             </div>
+            <h3>Erin Kristoff</h3>
+          </div>
+        </div>
 
-            <div class="text-wrap">
-              <h3>Founder B</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non sodales neque sodales ut etiam. Eleifend donec pretium vulputate sapien nec. Arcu risus quis varius quam quisque id. Mauris sit amet massa vitae tortor condimentum lacinia quis vel. Vehicula ipsum a arcu cursus vitae congue mauris. Duis tristique sollicitudin nibh sit amet commodo nulla. Facilisis volutpat est velit egestas dui id ornare arcu odio. Vitae aliquet nec ullamcorper sit amet risus nullam eget. Sapien eget mi proin sed libero enim. Euismod lacinia at quis risus sed vulputate odio. Tempus egestas sed sed risus pretium quam vulputate. Id diam vel quam elementum. Sagittis aliquam malesuada bibendum arcu vitae elementum curabitur.</p>
+        <div class="row">
+          <div class="col">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non sodales neque sodales ut etiam. Eleifend donec pretium vulputate sapien nec. Arcu risus quis varius quam quisque id. Mauris sit amet massa vitae tortor condimentum lacinia quis vel. Vehicula ipsum a arcu cursus vitae congue mauris. Duis tristique sollicitudin nibh sit amet commodo nulla. Facilisis volutpat est velit egestas dui id ornare arcu odio.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="social">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <h3>Follow Along</h3>
+            <div class="social-icons">
+              <a href="#"><i class="fab fa-facebook"></i></a>
+              <a href="#"><i class="fab fa-instagram"></i></a>
+              <a href="#"><i class="fab fa-twitter"></i></a>
             </div>
           </div>
         </div>
       </div>
     </section>
 
-    <!-- <div class="container">
-
-      <div class="row">
-        <div class="col-12 text-center">
-          
-        </div>
-      </div>
-      
-      <div class="row">
-        <div class="col-12">
-          <CardTeam 
-            name="Alessio Napoli"
-            image="Alessio Napoli.jpg"
-            job="Full Stack Web Developer"
-            linkGitHub="https://github.com/Alessio2299"
-          />
-        </div>
-        <div class="col-12">
-          <CardTeam 
-          name="Edoardo Lai"
-          image="Edoardo Lai.jpeg"
-          job="Full Stack Web Developer"
-          linkGitHub="https://github.com/EdoardoLai96"
-          />
-        </div>
-        
-        <div class="col-12">
-          <CardTeam 
-          name="Dario La Tegola"
-          image="Dario La Tegola.jpeg"
-          job="Full Stack Web Developer"
-          linkGitHub="https://github.com/Afrodario"
-          />
-        </div>
-
-        <div class="col-12">
-          <CardTeam 
-          name="Christian D'Agostino"
-          image="Christian D'Agostino.jpeg"
-          job="Full Stack Web Developer"
-          linkGitHub="https://github.com/Chris-dagostino"
-          />
-        </div>
-      </div>
-      
-    </div> -->
   </div>
   
 </template>
 
 <script>
-  import CardTeam from './partials/CardTeam.vue'
   export default {
-    name : 'Home',
-    components:{
-      CardTeam,
-    }
+    name : 'About',
   }
 </script>
 
@@ -161,22 +127,37 @@
 
   #head {
     height: 770px;
-    text-align: center;
-    background-color: $orange_primary;
-    display: flex;
-    align-items: center;
     position: relative;
+    background-image: url('../../../storage/app/public/img/head-santorini.jpg');
+    background-size: cover;
+    background-position: center;
 
-    .title.up {
-      color: $ms_dark;
-    }
+    .titles-wrap {
+      text-align: center;
+      position: absolute;
+      top: 170px;
+      left: 50%;
+      transform: translateX(-50%);
 
-    .title.down {
-      font-family: 'Brush Script MT', cursive;
-      font-size: 4rem;
-      font-weight: 400;
-      color: $ms_light;
-      text-transform: lowercase;
+      &:hover {
+        cursor: default;
+      }
+      
+      .title.up {
+        color: $ms_dark;
+      }
+
+      .title.down {
+        font-family: 'Brush Script MT', cursive;
+        font-size: 4rem;
+        font-weight: 400;
+        color: $ms_light;
+        text-transform: lowercase;
+
+        &:hover {
+          color: $ms_dark
+        }
+      }
     }
 
     .go-to-wrap {
@@ -187,26 +168,28 @@
       display: flex;
       flex-direction: column;
       align-items: center;
+      color: $ms-light;
 
       span {
         text-transform: uppercase;
         margin-bottom: 10px;
+        font-weight: 800;
       }
 
       .go-to-mission {
         width: 30px;
         height: 30px;
         border-radius: 50%;
-        border: 1px solid $ms_dark;
+        border: 2px solid $ms_light;
         display: flex;
         justify-content: center;
         align-items: center;
         font-size: 1.2rem;
         text-decoration: none;
-        color: $ms_dark;
+        color: inherit;
 
         &:hover {
-          color: $orange_secondary;
+          color: $ms_dark;
         }
       }
     }
@@ -217,21 +200,27 @@
     padding-bottom: 90px;
     background-color: $ms_light;
 
-    .col-4 {
+    .col {
       padding: 0px 25px;
 
       .img-wrap {
         width: 100%;
         height: 250px;
-        background-color: $orange_primary;
         border-radius: 20px;
         overflow: hidden;
+        transition: 0.3s;
 
         img {
           width: 100%;
           height: 100%;
           object-fit: cover;
-          object-position: center;
+          object-position: 50% 50%;
+          overflow: hidden;
+        }
+
+        &:hover {
+          transition: 0.4s;
+          transform: translateY(15px);
         }
       }
 
@@ -250,45 +239,30 @@
     }
   }
 
-  #story {
-    background-color: $blue_primary;
-    color: $ms_light;
-    position: relative;
+  #journey {
+    display: flex;
+    height: 540px;
 
-    .background-wrap {
-      width: 50%;
-      height: 100%;
-      background-image: url('../../../storage/app/public/img/waterfall.jpg');
-      background-position: right bottom;
+    .left {
+      background-image: url('../../../storage/app/public/img/journey-2.jpg');
       background-size: cover;
-      position: absolute;
-      top: 0;
-      left: 0;
-
-      &:before {
-        content: '';
-          position: absolute;
-          top: 0;
-          right: 0;
-          bottom: 0;
-          left: 0;
-          background-image: linear-gradient(90deg, rgba(0,0,0,0.6) 100%, rgba(0,212,255,1) 100%);
-          opacity: .6;
-      }
+      background-position: center;
+      width: 50%;
     }
 
-    .container {
-      padding: 80px 0px;
+    .right {
+      width: 50%;
       height: 100%;
+      background-color: $blue_secondary;
+      padding: 0px 30px;
+      display: flex;
+      align-items: center;
 
-      .row {
-        display: flex;
-        justify-content: center;
+      .text-wrap {
         text-align: center;
-        text-shadow: 1px 1px $ms_dark;
 
         p {
-          margin-top: 50px;
+          margin-top: 30px;
         }
       }
     }
@@ -299,43 +273,116 @@
     padding-bottom: 90px;
     background-color: $ms_light;
 
+    h1 {
+      text-align: center;
+      margin-bottom: 50px;
+      font-family: 'Brush Script MT', cursive;
+      font-size: 4rem;
+      font-weight: 400;
+    }
+
     .row {
       display: flex;
       justify-content: center;
 
-      .col-5 {
+      .col {
         display: flex;
         flex-direction: column;
-        padding: 0px 40px;
-      }
+        justify-content: center;
+        
+        .img-wrap {
+          height: 150px;
+          width: 150px;
+          border-radius: 50%;
+          background-color: $blue_secondary;
+          align-self: center;
+          overflow: hidden;
+          transition: 0.3s;
 
-      .img-wrap {
-        height: 150px;
-        width: 150px;
-        border-radius: 50%;
-        background-color: $blue_secondary;
-        align-self: center;
-        overflow: hidden;
+          &:hover {
+            box-shadow: 12px -1px #E2E4E3;
+            transition: 0.5s;
+          }
+        }
 
         img {
           width: 100%;
           height: 100%;
           object-fit: cover;
         }
-      }
-
-      .text-wrap {
 
         h3 {
-          margin-top: 30px;
+          margin-top: 15px;
           font-weight: 600;
           text-align: center;
+          font-family: 'Brush Script MT', cursive;
+          font-size: 2rem;
         }
 
         p {
           margin-top: 20px;
+          text-align: center;
         }
       }
     }
   }
-</style>>
+
+  #social {
+    background-color: $blue_secondary;
+    padding: 80px 0px;
+
+    .col-12 {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      h3 {
+        font-weight: 600;
+        padding: 0 20px;
+
+        &:after {
+          content: "";
+          display: block;
+          width: 100%;
+          border-bottom: 0.5px solid $ms_dark;
+          color: $ms_dark;
+          margin: 20px 0px;
+        }
+      }
+
+      .social-icons {
+        font-size: 1.7rem;
+
+        a {
+          margin-right: 15px;
+          text-decoration: none;
+          color: inherit;
+        }
+      }
+    }
+  }
+  
+  @media (max-width: 768px) {
+    #journey {
+      flex-direction: column;
+      height: 800px;
+
+      .left {
+        height: 50%;
+        width: 100%;
+        background-position: center;
+      }
+
+      .right {
+        height: 50%;
+        width: 100%;
+      }
+    }
+  }
+
+  @media (max-width: 576px) {
+    #journey {
+      height: 1000px;
+    }
+  }
+</style>
